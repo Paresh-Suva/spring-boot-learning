a98bfd41f876d6368f485e050f03c2fe423321d1
Paresh <paresh07suva@gmail.com>
2026-04-03 10:21:09 +0530

    Added JWT

---
+++ 05-security/jwt-auth-demo/src/main/java/com/example/jwt_auth/JwtAuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthDemoApplication.class, args);
+	}
+
+}
